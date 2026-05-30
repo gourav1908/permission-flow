ff8429cbcdec1a134c97506570bdab0ddc8beb65
Gourav Bhatnagar <gouravofficial19@gmail.com>
2026-05-30 19:49:43 +0530

    Fix Maven Central credentials mapping

--- permissionflow/build.gradle.kts
+++ permissionflow/build.gradle.kts
@@ -1,12 +1,13 @@
-import java.util.Base64
-
 plugins {
     alias(libs.plugins.android.library)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
-    id("maven-publish")
+    id("com.vanniktech.maven.publish") version "0.36.0"
     id("signing")
 }
+
+group = "io.github.gourav1908"
+version = "1.0.0-alpha02"
 
 android {
     namespace = "io.github.permissionflow"
@@ -44,44 +45,29 @@ android {
     composeOptions {
         kotlinCompilerExtensionVersion = "1.5.15"
     }
-
-    publishing {
-        singleVariant("release") {
-            withSourcesJar()
-        }
-    }
 }
 
-signing {
-    val fileSigningKey = providers.gradleProperty("signing.keyFile")
-        .orElse(providers.environmentVariable("SIGNING_KEY_FILE"))
-        .orNull
-        ?.let { keyFile ->
-            file(keyFile).readText()
-        }
-    val plainSigningKey = providers.gradleProperty("signing.key")
-        .orElse(providers.environmentVariable("SIGNING_KEY"))
-        .orNull
-    val base64SigningKey = providers.environmentVariable("SIGNING_KEY_BASE64")
-        .orNull
-    val signingKey = (fileSigningKey ?: base64SigningKey?.let { encodedKey ->
-            String(
-                Base64.getDecoder().decode(encodedKey)
-            )
-        } ?: plainSigningKey)
-        ?.replace("\\n", "\n")
-    val signingPassword = providers.gradleProperty("signing.password")
-        .orElse(providers.environmentVariable("SIGNING_PASSWORD"))
-        .orNull
+mavenPublishing {
+    publishToMavenCentral()
+    signAllPublications()
 
-    if (!signingKey.isNullOrBlank() &&
-        !signingPassword.isNullOrBlank()
-    ) {
-        useInMemoryPgpKeys(
-            signingKey,
-            signingPassword
+    coordinates(
+        groupId = "io.github.gourav1908",
+        artifactId = "permission-flow",
+        version = "1.0.0-alpha02"
+    )
+
+    pom {
+
+        name.set("Permission Flow")
+
+        description.set(
+            "Modern Compose-first Android permission manager SDK"
         )
-        sign(publishing.publications)
+
+        url.set(
+            "https://github.com/gourav1908/permission-flow"
+        )
     }
 }
 
@@ -102,107 +88,4 @@ dependencies {
     implementation(libs.androidx.activity.compose)
     // Coroutines
     implementation(libs.kotlinx.coroutines.android)
-}
-
-afterEvaluate {
-
-    publishing {
-
-        publications {
-
-            create<MavenPublication>("release") {
-
-                from(components["release"])
-
-                groupId = "io.github.gourav1908"
-
-                artifactId = "permission-flow"
-
-                version = "1.0.0-alpha01"
-
-                pom {
-
-                    name.set("Permission Flow")
-
-                    description.set(
-                        "Modern Compose-first Android permission manager SDK"
-                    )
-
-                    url.set(
-                        "https://github.com/gourav1908/permission-flow"
-                    )
-
-                    licenses {
-
-                        license {
-
-                            name.set("Apache-2.0")
-
-                            url.set(
-                                "https://opensource.org/licenses/Apache-2.0"
-                            )
-                        }
-                    }
-
-                    developers {
-
-                        developer {
-
-                            id.set("gourav1908")
-
-                            name.set("Gourav Bhatnagar")
-                        }
-                    }
-
-                    scm {
-
-                        connection.set(
-                            "scm:git:git://github.com/gourav1908/permission-flow.git"
-                        )
-
-                        developerConnection.set(
-                            "scm:git:ssh://github.com/gourav1908/permission-flow.git"
-                        )
-
-                        url.set(
-                            "https://github.com/gourav1908/permission-flow"
-                        )
-                    }
-                }
-            }
-        }
-
-        repositories {
-
-            maven {
-
-                name = "MavenCentral"
-
-                url = uri(
-                    "https://central.sonatype.com/api/v1/publisher/upload"
-                )
-
-                credentials {
-
-                    username =
-                        providers.gradleProperty(
-                            "mavenCentralUsername"
-                        ).orElse(
-                            providers.environmentVariable(
-                                "MAVEN_CENTRAL_USERNAME"
-                            )
-                        ).getOrElse("")
-
-                    password =
-                        providers.gradleProperty(
-                            "mavenCentralPassword"
-                        ).orElse(
-                            providers.environmentVariable(
-                                "MAVEN_CENTRAL_PASSWORD"
-                            )
-                        ).getOrElse("")
-                }
-            }
-        }
-    }
 }
